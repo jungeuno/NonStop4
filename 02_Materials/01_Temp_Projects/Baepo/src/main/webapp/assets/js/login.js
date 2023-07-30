d292ec12ae4b69a8388393d37b5bf6daf13d0c26
hj <hj@hj-4.local>
2023-07-30 18:36:53 +0900

    AHN 230730 login.js 수정

--- 02_Materials/01_Temp_Projects/Baepo/src/main/webapp/assets/js/login.js
+++ 02_Materials/01_Temp_Projects/Baepo/src/main/webapp/assets/js/login.js
@@ -6,65 +6,27 @@ window.TrackJS &&
 function saveUserInfo(){
     //save user-id 
 }
+
+async function login(baseURL){
+    console.log(`login func 진입`);
+    const requestURI = `/login`;
+    const url = baseURL + requestURI;
+    try{
+        const response=await fetch(url,{
+            method: 'GET',
+        });
+    } catch(error) {
+        console.log(`login 함수에서 에러 발생 :\n${error}`);
+    }
+}
+
 $(document).ready(function() {
-
     $().ready(function() {
-        $sidebar = $('.sidebar');
-        $navbar = $('.navbar');
-        $main_panel = $('.main-panel');
+        const baseURL = window.location.origin;
+        const loginButton=document.querySelector("#login-button");
+        console.log(loginButton);
 
-        $full_page = $('.full-page');
-
-        $sidebar_responsive = $('body > .navbar-collapse');
-        sidebar_mini_active = true;
-        white_color = false;
-
-        window_width = $(window).width();
-
-        fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
-
-        $('.switch-sidebar-mini input').on("switchChange.bootstrapSwitch", function() {
-            var $btn = $(this);
-
-            if (sidebar_mini_active == true) {
-                $('body').removeClass('sidebar-mini');
-                sidebar_mini_active = false;
-                blackDashboard.showSidebarMessage('Sidebar mini deactivated...');
-            } else {
-                $('body').addClass('sidebar-mini');
-                sidebar_mini_active = true;
-                blackDashboard.showSidebarMessage('Sidebar mini activated...');
-            }
-
-            // we simulate the window Resize so the charts will get updated in realtime.
-            var simulateWindowResize = setInterval(function() {
-                window.dispatchEvent(new Event('resize'));
-            }, 180);
-
-            // we stop the simulation of Window Resize after the animations are completed
-            setTimeout(function() {
-                clearInterval(simulateWindowResize);
-            }, 1000);
-        });
-
-        $('.switch-change-color input').on("switchChange.bootstrapSwitch", function() {
-            var $btn = $(this);
-            if (white_color == true) {
-                $('body').addClass('change-background');
-                setTimeout(function() {
-                    $('body').removeClass('change-background');
-                    $('body').removeClass('white-content');
-                }, 900);
-                white_color = false;
-            } else {
-                $('body').addClass('change-background');
-                setTimeout(function() {
-                    $('body').removeClass('change-background');
-                    $('body').addClass('white-content');
-                }, 900);
-                white_color = true;
-            }
-        });
+        loginButton.addEventListener("click",login(baseURL));
 
     });
 });
