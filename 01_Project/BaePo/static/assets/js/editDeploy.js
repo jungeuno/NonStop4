906fbebbc6791fccf1e83b1c58905b91cdaa1fd6
hj <hj@hj-4.local>
2023-08-05 23:19:01 +0900

    230805 AHN editDeploy 화면 수정, 요청 함수 수정

--- 01_Project/BaePo/static/assets/js/editDeploy.js
+++ 01_Project/BaePo/static/assets/js/editDeploy.js
@@ -1,27 +1,58 @@
 const baseURL=window.location.origin;
 const KEY_USER_EMAIL="user-email";
 const KEY_SERVICE_NAME="service-name";
+const SERVICE_EDIT_BUTTONS_CLASS="service-edit-buttons";
 const formElemnt=document.querySelector("form#editDeployForm");
 
-function handleUpdateButtonClick(){
-    const requestURI=`/users/${userEmail}/services/${serviceName}`
-
-
+async function handleServiceEditButtonClick(event){  
+    //button의 value : {UPDATE / DELETE} 에 따라서 서비스를 업데이트하거나 삭제하는 요청을 보냄
+    const requestURI=`/services/${serviceName}`
+    const url=baseURL+requestURI;
+    const method=event.target.value;
+    const options={
+        method:{method}
+    }
+    console.log(`handleServiceEditButtonClick : ${options}`);
+    try{
+        const response=await fetch(url,options);
+        if(response.ok){
+            location.href="containerList.html";
+        }
+        else{
+            console.log(`${url}로 ${options.method}요청 서버의 비정상 응답 : [${response.status}] ${response.statusText}`);
+        }
+    } catch(error){
+        console.log(`${url}로 ${options.method}요청 작업 중 에러 발생 : \n${error}`);
+    }
 }
-function handleDeleteButtonClick(){
 
-}
 $(document).ready(function() {
     $().ready(function() {
         const userEmail=localStorage.getItem(KEY_USER_EMAIL);
         const serviceName=localStorage.getItem(KEY_SERVICE_NAME);
-        const updateButton=document.querySelector("button#updateButton");
-        const deleteButton=document.querySelector("button#deleteButton");
 
-        updateButton.addEventListener("click",handleUpdateButtonClick);
-        deleteButton.addEventListener("click",handleDeleteButtonClick);
+        const serviceEditButtons=document.querySelectorAll(`.${SERVICE_EDIT_BUTTONS_CLASS}`);
+        serviceEditButtons.forEach((serviceEditButton)=>serviceEditButton.addEventListener(handleServiceEditButtonClick));
 
-        $('#devEnv').bsMultiSelect({
+        $('#frontEnv').bsMultiSelect({
+            useCssPatch:true,
+            cssPatch:{
+                choices:{
+                    columnCount:'4'
+                }
+            },
+        }); //multi select plugin
+
+        $('#dbEnv').bsMultiSelect({
+            useCssPatch:true,
+            cssPatch:{
+                choices:{
+                    columnCount:'4'
+                }
+            },
+        }); //multi select plugin
+
+        $('#backEnv').bsMultiSelect({
             useCssPatch:true,
             cssPatch:{
                 choices:{
@@ -38,6 +69,5 @@ $(document).ready(function() {
                 }
             },
         }); //multi select plugin
-
     });
 });
