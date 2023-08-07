1e8235a968862e9c9768f456f5aa10e3ffcf3df9
jungeuno <jungeuno76@gmail.com>
2023-08-08 05:13:53 +0900

    230808 OH main.py 수정 & user.json 파일 생성 코드 작성

--- 01_Project/BaePo/main.py
+++ 01_Project/BaePo/main.py
@@ -13,14 +13,14 @@ app = Flask(__name__)
 BASE_DIR = os.getcwd()
 
 # 구글 로그인 API 클라이언트 ID & PASSWORD
-app.config['SECRET_KEY'] = 'AIzaSyCHUx1bpNdKFMJ_3lvwaCbw15PabF1JNpw'
+app.config['SECRET_KEY'] = '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'
 app.config['GOOGLE_OAUTH2_CLIENT_SECRETS_FILE'] = 'client_secret_9542596386-2kkmu7fkdv00p6pomousdniphu0job4i.apps.googleusercontent.com.json'
 
 oauth2 = UserOAuth2(app)
 
-# 가입 날짜 기록 변수 - today_date
+# 현재 날짜/시간 기록 변수 - today_date
 now = dt.datetime.now()
-today_date = now.date()
+today_date = now.strftime('%Y-%m-%d | %H:%M:%S')
 
 # JSON 파일 경로
 data_file_path = 'data.json'
@@ -29,6 +29,14 @@ user_data = {}
 if os.path.exists(data_file_path):
     with open(data_file_path, 'r', encoding='utf-8') as fp:
         user_data = json.load(fp)
+
+# 'user.json'JSON 파일 경로
+user_file_path = 'user.json'
+# JSON 파일을 읽어서 데이터를 저장할 딕셔너리 생성
+user_list = {}
+if os.path.exists(user_file_path):
+    with open(user_file_path, 'r', encoding='utf-8') as fp:
+        user_list = json.load(fp)
 ######################################################################################################################################
 # route 설정 & 페이지 렌더링
 @app.route('/')
@@ -79,13 +87,16 @@ def login():
     credentials = oauth2.credentials
     # 로그인 계정의 이메일과 사용자 id를 받아옴
 
-    if oauth2.has_credentials():
-        # 로그인 성공 시, useremail을 리스트에 저장
-        # useremail = oauth2.email
-        return render_template('containerList.html', useremail=oauth2.email)
-    else:
-        return render_template('register.html')
+    # user_list에 사용자 이메일이 없다면 JSON 파일에 가입 일자 등록 
+    if oauth2.email not in user_list:
+        registerationDate = today_date
+        user_list[oauth2.email] = [{
+            "Registeration Date": registerationDate }]
+        with open(user_file_path, 'w', encoding='utf-8') as fp:
+            json.dump(user_list, fp, sort_keys=True, indent=4, ensure_ascii=False)
 
+    return render_template('containerList.html', useremail=oauth2.email)
+######################################################################################################################################
 # 로그아웃
 # logout 버튼 클릭 시, 실행되도록 프론트와 연동해야 함.    !!!!!!!!!!!!!!!!!!!!!!!!
 @app.route('/logout', methods=['POST', 'GET'])
@@ -184,13 +195,13 @@ def containerDeploy_page():
             user_data[oauth2.email].append({
                 'Service Name': program_name,
                 'Containers' : containers,
-                'Creating Date' : str(today_date)
+                'Creating Date' : today_date
             })
         else:
             user_data[oauth2.email] = [{
                 'Service Name': program_name,
                 'Containers' : containers,
-                'Creating Date' : str(today_date)
+                'Creating Date' : today_date
             }]
 
         # JSON 파일에 데이터를 저장 (ensure_ascii 옵션을 False로 설정하여 한글이 유니코드로 저장되도록 함)
@@ -207,52 +218,15 @@ def containerDeploy_page():
     #     return render_template('editDeploy.html', useremail=oauth2.email)
 ######################################################################################################################################
 # @app.route('/editDeploy.html')
-@app.route('/services/<string:service_name>')
+@app.route('/services/<string:service_name>', methods=['POST', 'GET'])
 def containerEditDeploy_page(service_name):
     user_email = oauth2.email                            # 현재 로그인된 사용자 이메일
     if request.method == 'POST':
         file = request.files['uploadFile_name']          # 업로드되는 파일 받기
         program_name = service_name                      # 사용자가 배포하는 프로그램명 받기
-        front_env = request.form.getlist('frontEnv')     # 선택된 개발 환경 리스트 받기
-        back_env = request.form.getlist('dbEnv')         # 선택된 개발 환경 리스트 받기
-        db_env = request.form.getlist('backEnv')         # 선택된 개발 환경 리스트 받기
-
-        # json 데이터 초기화
-        containers = []
-        envx = [0]*16
-        front = {}
-        back = {}
-        db = {}
-        # 프론트/백/DB -> json 파일에 서비스명 + 컨테이너명(서비스명_Front/서비스면_Back/서비스명_DB) 설정
-        if front_env is not None:
-                front['name'] = program_name+'_Front'
-                front['state'] = 'run'
-        if db_env is not None:
-                db['name'] = program_name+'_Db'
-                db['state'] = 'run'
-        if back_env is not None:
-                back['name'] = program_name+'_Back'
-                back['state'] = 'run'
-        # json 파일에 env (개발환경) 설정
-        for fe in front_env:
-            fe = int(fe)
-            envx[fe] = 1
-        for de in db_env:
-            de = int(de)    
-            envx[de] = 1
-        for be in back_env:
-            be = int(be)        
-            envx[be] = 1
-        print('envx', envx)
-
-        # json 파일의 containers 객체 설정
-        front['env'] = front_env
-        back['env'] = back_env
-        db['env'] = db_env
-        containers.append(front)
-        containers.append(back)
-        containers.append(db)
-
+        print(service_name)# 서비스명 값 받아왔는지 확인
+        print(program_name)
+        
         # # BASE 경로 -> {현재 실행되는 path}/userSource/{user_email}
         # folder_path = os.path.join(BASE_DIR, 'userSource', user_email)
         # os.makedirs(folder_path, exist_ok=True)
@@ -289,13 +263,12 @@ def containerEditDeploy_page(service_name):
         # 사용자 이메일을 기준으로 데이터가 있는지 확인하고 데이터 추가 또는 새로운 사용자 JSON 객체 생성
         if program_name in user_data[oauth2.email]['Service Name']:
             for ud in user_data[oauth2.email]:
-                 if ud['Service Name'] == program_name:
-                    ud['Containers'] = containers
-                    # user_data[oauth2.email]['Service Name'] = program_name
-                    # user_data[oauth2.email]['Containers'] = containers
-                    # user_data[oauth2.email]['Creating Date'] = str(today_date)
+                if ud['Service Name'] == program_name:
+                    ud['Service Name'].append({
+                    'Update': today_date})
                     print(ud['Service Name'])
                     print(ud['Containers'])
+                    print(ud)
         else:
             return '기존의 서비스 명과 동일하지 않습니다.'
 
@@ -310,7 +283,6 @@ def containerEditDeploy_page(service_name):
     elif request.method == 'GET':
         # state 수정해주어야 함.
         return json.dumps({oauth2.email: user_data[oauth2.email]}, ensure_ascii=False)
-    return 'Fail'
 ######################################################################################################################################
 # 컨테이너 status 확인 (Running/Stop/Error?) / pip install paramiko
 # def container_status():
