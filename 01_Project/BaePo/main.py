8cb188c899c2807af68d5af5d9e9851248e18ab9
jungeuno <jungeuno76@gmail.com>
2023-08-08 22:49:37 +0900

    230808 OH main.py 수정'

--- 01_Project/BaePo/main.py
+++ 01_Project/BaePo/main.py
@@ -20,7 +20,7 @@ oauth2 = UserOAuth2(app)
 
 # 현재 날짜/시간 기록 변수 - today_date
 now = dt.datetime.now()
-today_date = now.strftime('%Y-%m-%d  |  %H:%M:%S')
+today_date = now.strftime('%Y - %m - %d    |    %H : %M : %S')
 
 # JSON 파일 경로
 data_file_path = 'data.json'
@@ -70,18 +70,7 @@ def containerDash_page():
 def deploy_page():
     if oauth2.has_credentials():
         return render_template('deploy.html', useremail=oauth2.email)
-    
-@app.route('/mypage.html')
-def my_page():
-    if oauth2.has_credentials():
-        user_email = oauth2.email               # 사용자 이메일
-        user_date = user_list.get(user_email)   # 사용자 데이터 가져오기
-        user_registeration = user_date[0]["Registeration Date"]
 
-        if user_email is None or user_data is None:
-            return "User not found"
-        return render_template('mypage.html', useremail=user_email, userDate=user_registeration)
-######################################################################################################################################
 # 로그인
 @app.route('/login', methods=['GET', 'POST'])
 @oauth2.required
@@ -106,6 +95,18 @@ def logout():
     session.clear()
     return render_template('login.html')
 ######################################################################################################################################
+# 마이페이지
+@app.route('/mypage.html')
+def my_page():
+    if oauth2.has_credentials():
+        user_email = oauth2.email               # 사용자 이메일
+        user_date = user_list.get(user_email)   # 사용자 데이터 가져오기
+        user_registeration = user_date[0]["Registeration Date"]
+
+        if user_email is None or user_data is None:
+            return "User not found"
+        return render_template('mypage.html', useremail=user_email, userDate=user_registeration)
+######################################################################################################################################
 # 사용자별 배포 목록
 # 사용자 이름과 배포명 추가 -> json 파일로 저장
 # 사용자 이름 해당하면 배포명 (리스트) json dump 반환
@@ -119,26 +120,39 @@ def containerDeploy_page():
         file = request.files['uploadFile_name']          # 업로드되는 파일 받기
         program_name = request.form['service_name']      # 사용자가 배포하는 프로그램명 받기
         front_env = request.form.getlist('frontEnv')     # 선택된 개발 환경 리스트 받기
-        back_env = request.form.getlist('dbEnv')         # 선택된 개발 환경 리스트 받기
-        db_env = request.form.getlist('backEnv')         # 선택된 개발 환경 리스트 받기
-
+        db_env = request.form.getlist('dbEnv')         # 선택된 개발 환경 리스트 받기
+        back_env = request.form.getlist('backEnv')         # 선택된 개발 환경 리스트 받기
+        
         # json 데이터 초기화
         containers = []
         envx = [0]*16
         front = {}
         back = {}
         db = {}
+        frontStatus = []
+        backStatuse = []
+        dbStatus = []
         # 프론트/백/DB -> json 파일에 서비스명 + 컨테이너명(서비스명_Front/서비스면_Back/서비스명_DB) 설정
+        # 'state' 컨테이너 status 값 파싱 & JSON 파일에 저장
+        result_dict = control_containers(user_email+':'+program_name)
+        for dict_name in result_dict:
+            if 'frontend-deployment' in dict_name:
+                frontStatus.append(result_dict[dict_name])
+            elif 'backend-deployment' in dict_name:
+                backStatuse.append(result_dict[dict_name])
+            elif 'db-deployment' in dict_name:
+                dbStatus.append(result_dict[dict_name])
+        front['State'] = frontStatus
+        back['State'] = backStatuse
+        db['State'] = dbStatus
+        # 'name'
         if front_env is not None:
                 front['name'] = program_name+'_Front'
-                front['state'] = 'run'
         if db_env is not None:
                 db['name'] = program_name+'_Db'
-                db['state'] = 'run'
         if back_env is not None:
                 back['name'] = program_name+'_Back'
-                back['state'] = 'run'
-        # json 파일에 env (개발환경) 설정
+        # 'env' (개발환경) 설정
         for fe in front_env:
             fe = int(fe)
             envx[fe] = 1
@@ -157,6 +171,24 @@ def containerDeploy_page():
         containers.append(front)
         containers.append(back)
         containers.append(db)
+
+        # 사용자 이메일을 기준으로 데이터가 있는지 확인하고 데이터 추가 또는 새로운 사용자 JSON 객체 생성
+        if oauth2.email in user_data:
+            user_data[oauth2.email].append({
+                'Service Name': program_name,
+                'Containers' : containers,
+                'Creating Date' : today_date
+            })
+        else:
+            user_data[oauth2.email] = [{
+                'Service Name': program_name,
+                'Containers' : containers,
+                'Creating Date' : today_date
+            }]
+
+        # JSON 파일에 데이터를 저장 (ensure_ascii 옵션을 False로 설정하여 한글이 유니코드로 저장되도록 함)
+        with open(data_file_path, 'w', encoding='utf-8') as fp:
+            json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False)
 
         # # BASE 경로 -> {현재 실행되는 path}/userSource/{user_email}
         # folder_path = os.path.join(BASE_DIR, 'userSource', user_email)
@@ -186,28 +218,10 @@ def containerDeploy_page():
         #         f.write(username_txt)
         #     os.remove(file_path)  # .zip 파일 삭제
                     
-        #     # GitHub에 업로드
-        #     # upload_to_github(os.path.join(BASE_DIR, 'userSource'))
-        # else:
-        #     return '.zip 파일을 업로드 해주세요.'
-
-        # 사용자 이메일을 기준으로 데이터가 있는지 확인하고 데이터 추가 또는 새로운 사용자 JSON 객체 생성
-        if oauth2.email in user_data:
-            user_data[oauth2.email].append({
-                'Service Name': program_name,
-                'Containers' : containers,
-                'Creating Date' : today_date
-            })
-        else:
-            user_data[oauth2.email] = [{
-                'Service Name': program_name,
-                'Containers' : containers,
-                'Creating Date' : today_date
-            }]
-
-        # JSON 파일에 데이터를 저장 (ensure_ascii 옵션을 False로 설정하여 한글이 유니코드로 저장되도록 함)
-        with open(data_file_path, 'w', encoding='utf-8') as fp:
-            json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+        # #     # GitHub에 업로드
+        # #     # upload_to_github(os.path.join(BASE_DIR, 'userSource'))
+        # # else:
+        # #     return '.zip 파일을 업로드 해주세요.'
 
         # 응답으로 JSON 형식의 데이터 반환
         return render_template('containerList.html', userData=json.dumps({user_email: user_data[user_email]}, ensure_ascii=False))
@@ -231,13 +245,27 @@ def containerEditDeploy_page(service_name):
 
         # 해당 사용자의 컨테이너 리스트 데이터 가져오기
         getUserData = user_data.get(user_email)
-        print('GetUserData :',getUserData)
-        print('GetUserData[0] :', getUserData[0])
+        # 컨테이너 status 값 파싱 & JSON 파일에 저장
+        result_dict = control_containers(user_email+':'+program_name)
         for element in getUserData:
             if element['Service Name'] == program_name:
                 element['Update'] = today_date
+
+                for dict_name in result_dict:
+                    if 'frontend-deployment' in dict_name:
+                        element['Containers'][0] = result_dict[dict_name]
+                    elif 'backend-deployment' in dict_name:
+                        element['Containers'][1] = result_dict[dict_name]
+                    else:
+                        element['Containers'][2] = result_dict[dict_name]
             else:
-                return '배포하려는 서비스와 다릅니다.'
+                return '업데이트 실패'
+
+        # JSON 파일에 데이터를 저장 (ensure_ascii 옵션을 False로 설정하여 한글이 유니코드로 저장되도록 함)
+        with open(data_file_path, 'w', encoding='utf-8') as fp:
+            json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+
+        print(json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False))
 
         # # BASE 경로 -> {현재 실행되는 path}/userSource/{user_email}
         # folder_path = os.path.join(BASE_DIR, 'userSource', user_email)
@@ -252,19 +280,6 @@ def containerEditDeploy_page(service_name):
         #     unzip_folder_path = os.path.splitext(file_path)[0]  # .zip 확장자 제외한 경로
         #     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         #         zip_ref.extractall(unzip_folder_path)
-        #     # 체크한 개발 환경 기반으로 env_txt 파일 생성
-        #     env_txt = ''
-        #     for e in envx:
-        #         env_txt += str(e)
-        #     # 체크한 개발 환경 기반으로 env_txt 파일 저장
-        #     env_txt_file_path = os.path.join(unzip_folder_path, 'env.txt')
-        #     with open(env_txt_file_path, 'w') as f:
-        #         f.write(env_txt)
-        #     # username.txt 파일 생성 및 저장
-        #     username_txt = user_email+':'+program_name
-        #     username_txt_file_path = os.path.join(unzip_folder_path, 'username.txt')
-        #     with open(username_txt_file_path, 'w', encoding='utf-8') as f:
-        #         f.write(username_txt)
         #     os.remove(file_path)  # .zip 파일 삭제
                     
         #     # GitHub에 업로드
@@ -272,55 +287,48 @@ def containerEditDeploy_page(service_name):
         # else:
         #     return '.zip 파일을 업로드 해주세요.'
 
-        # JSON 파일에 데이터를 저장 (ensure_ascii 옵션을 False로 설정하여 한글이 유니코드로 저장되도록 함)
-        with open(data_file_path, 'w', encoding='utf-8') as fp:
-            json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False)
-
-        print(json.dump(user_data, fp, sort_keys=True, indent=4, ensure_ascii=False))
-
         # 응답으로 JSON 형식의 데이터 반환
         return render_template('containerList.html', userData=json.dumps({oauth2.email: user_data[oauth2.email]}, ensure_ascii=False))
     elif request.method == 'GET':
         # state 수정해주어야 함.
         return json.dumps({oauth2.email: user_data[oauth2.email]}, ensure_ascii=False)
 ######################################################################################################################################
-# 컨테이너 status 확인 (Running/Stop/Error?) / pip install paramiko
-# def container_status():
-#     ssh = paramiko.SSHClient()
-#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#     ssh.connect('150.136.87.94', port='22', username='opc', key_filename='./master-06-26.key')
+# 컨테이너 status 확인 (Running/Stop/Pending/error 등) / pip install paramiko
+def control_containers(namespace):
+    setUser = 'opc'          
+    namespace = 'buttontest' # 환경 변수
 
-#     stdin, stdout, stderr = ssh.exec_command('kubectl get pod -n test') # kubectl get deployment -n {$useremail}
-#     output_lines = stdout.readlines()  # Capture the output in a variable
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('150.136.87.94', port = '22', username = setUser, key_filename = './master-06-26.key')
 
-#     # Debug: print the output to check if it's as expected
-#     print("=== Output ===")
-#     print("".join(output_lines))
-#     print("==============")
+    stdin, stdout, stderr = ssh.exec_command('kubectl get pod -n '+ namespace) # kubectl get deployment -n {$namespace}
+    output_lines = stdout.readlines()  
+    
+    result = "".join(output_lines)
+    print("=== Output ===")
+    print(result)
+    print("==============")
 
-#     deployment_info = {}  # 딕셔너리로 저장할 변수
+    deployment_info = {}  # 딕셔너리로 저장할 변수
 
-#     # 첫 번째 줄은 헤더이므로 무시하고, 두 번째 줄부터 파싱 시작
-#     for line in output_lines[1:]:
-#         line = line.strip()  # 줄바꿈 문자 제거
-#         if not line:
-#             continue  # 빈 줄은 무시
+    # 첫 번째 줄은 헤더이므로 무시하고, 두 번째 줄부터 파싱 시작
+    for line in output_lines[1:]:
+        line = line.strip()  # 줄바꿈 문자 제거
+        if not line:
+            continue  # 빈 줄은 무시
 
-#         # 줄을 공백으로 분리하여 name과 status 정보 추출
-#         columns = line.split()
-#         name = columns[0]
-#         status = columns[2]
+        # 줄을 공백으로 분리하여 name과 status 정보 추출
+        columns = line.split()
+        name = columns[0]
+        status = columns[2]
 
-#         # 딕셔너리에 name을 키로, status를 값으로 저장
-#         deployment_info[name] = status
+        # 딕셔너리에 name을 키로, status를 값으로 저장
+        deployment_info[name] = status
 
-#     ssh.close()
+    ssh.close()
 
-#     return deployment_info  # 딕셔너리 반환
-
-# result_dict = control_containers()
-# print(result_dict)
-
+    return deployment_info # 딕셔너리 반환
 ######################################################################################################################################
 # 컨테이너 제어 작업 (stop/restart/delete) / pip install paramiko
 # @app.route('/controls', methods=['POST', 'GET'])
